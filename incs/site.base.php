8848646217ea4b7c6862d11cd0c0bc6a05f0e180
Steven Chin <e.nigma@inbox.com>
2015-05-29 16:13:24 -0500

    Revert "resume"
    This reverts commit 7454238a43f2edb3419cfc8578267f07352279f9.

--- incs/site.base.php
+++
@@ -1,9 +0,0 @@
-			</div><!-- /page -->
-		</div><!-- /container -->
-		</main>
-		<script type="text/javascript" src="js/vendor/jq/jquery.min.js"></script>
-		<script type="text/javascript" src="js/vendor/bs/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/vendor/pb/progressbar.min.js"></script>
-		<script type="text/javascript" src="js/cv.js"></script>
-	</body>
-</html>
