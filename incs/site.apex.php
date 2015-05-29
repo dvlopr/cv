7454238a43f2edb3419cfc8578267f07352279f9
Steven Chin <e.nigma@inbox.com>
2015-05-29 16:13:11 -0500

    resume

---
+++ incs/site.apex.php
@@ -0,0 +1,14 @@
+﻿<!DOCTYPE html>
+<?php include_once("cls/site.php"); ?>
+
+<html lang="en">
+	<head>
+		<title>Resume</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/vendor/bs/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<main>
+			<div class="container">
+				<div id="page">
